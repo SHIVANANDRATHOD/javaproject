1f0d9cb2fb42431d31657124c5e6a58aa6909ff1
shivanand <naik85303@gmail.com>
2023-02-19 17:37:25 +0530

    this is my java project

---
+++ src/gitdempproject1/test.java
@@ -0,0 +1,13 @@
+package gitdempproject1;
+
+public class test {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if (hungry=="yes")
+		    System.out.println("he will eat somithin");
+		 else {
+			System.out.println("i will do some work");	}
+	}
+
+}
